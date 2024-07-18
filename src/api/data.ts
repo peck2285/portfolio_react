7b8620e295ff0681f38d93f5e959179b54a3bc4e
zadi gbagbo <zadigbagboalexfranklin@gmail.com>
2024-07-18 23:57:00 +0800

    add react

--- src/api/data.ts
+++ src/api/data.ts
@@ -78,48 +78,73 @@ export const portofolios : Portofolio[] =[
     {
         title :"Template ecommerce",
         time : "2 semaine",
-        previewUrl : "https://jstores.netlify.app",
+        previewUrl : "https://ecommerce-wo6y.onrender.com",
         description :"Réalisation d'un template ecommerce  fais avec les languages suivant html CSS3 et JavaScript .",
         
     },
      {
         title :"Boulangerie responsive",
-        previewUrl: "https://pain-dore.netlify.app",
+        previewUrl: "https://boulangerie-js.onrender.com",
         time : "2 semaine",
-        description :"Réalisation d'une Boulangerie responsive fais en html et css .",
+        description :"Réalisation d'une Boulangerie responsive fais en html et css et javascript.",
         
     },
      {
+        title :"Project boostrap",
+        previewUrl: "https://projet-boostrap.onrender.com",
+        time : "2 heure ",
+        description :"Réalisation de ce projet boostrap ",
+        
+    },
+     
+    {
         title :"jeux JavaScript",
-        time : "2 semaine",
-        previewUrl: "https://gamelofts.netlify.app",
+        time : "1 jour",
+        previewUrl: "https://javascript-quiz-o9dr.onrender.com",
         description :"Réalisation d'un jeu en JavaScript  dans le quelle vous etes soumus a plusieurs questions pendants une période de 10s .",
         
     },
    
     {
         title :"application de gestion des  films",
-        previewUrl:"https://moviehandles.netlify.app",
-        time : "2 semaine",
+        previewUrl:"https://film-script.onrender.com",
+        time : "2 jour",
         description :"C'est une application qui permet de sélectionner des films de votre choix grace a une barre de recherche .",
        
     },
 
      {
         title :"Javascript filter",
-        previewUrl:"https://filterfil.netlify.app",
-        time : "2 semaine",
+        previewUrl:"https://javascript-filter.onrender.com",
+        time : "1 jour",
         description :"Une application constitué d'objet avec une barre de recherche pouvant les rechercher ces objets par leur nom ou leur catégorie",
+       
+    },
+
+     {
+        title :"TTODOLIST JAVASCRPT",
+        previewUrl:"https://javascript-taches.onrender.com",
+        time : "1 jour",
+        description :"Une application pouvantc crée des taches et les sauvegarder en local",
        
     },
 
     {
         title :"Gestion de bibliothèque avec php",
         time : "3 semaine",
-        previewUrl:"https://bookshandlers.netlify.app",
+        previewUrl:"https://github.com/peck2285/biblioth-que_php.git",
         description :" Une application consiste à créer un système de gestion de bibliothèque interactif et fonctionnel",
        
     },
+
+    {
+        title :"Gestion d'évènementavec php",
+        time : "3 semaine",
+        previewUrl:"https://github.com/peck2285/GestionEvents.git",
+        description :" Une application consiste à créer un système de gestion de évènement interactif et fonctionnel",
+       
+    },
+
 
 
     
