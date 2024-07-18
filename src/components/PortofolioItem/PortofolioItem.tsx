0c48ca5213049814a04077dff2e587564a0fc550
zadi gbagbo <zadigbagboalexfranklin@gmail.com>
2024-07-19 00:12:10 +0800

    add react app

--- src/components/PortofolioItem/PortofolioItem.tsx
+++ src/components/PortofolioItem/PortofolioItem.tsx
@@ -4,41 +4,42 @@
   App Name : E-commerce with React.Js
   Created At : 01/06/2024 21:20:28
 */
-import React, { FC, useEffect } from 'react';
-import './PortofolioItem.css';
-import { Portofolio } from '../../models/Portofolio';
-import { Link } from 'react-router-dom';
-
+import React, { FC, useEffect } from "react";
+import "./PortofolioItem.css";
+import { Portofolio } from "../../models/Portofolio";
 
 
 interface PortofolioItemProps {
- portofolio : Portofolio
+  portofolio: Portofolio;
 }
 
-
-const PortofolioItem : FC<PortofolioItemProps> = ({portofolio}) =>{
-
-
-
-    useEffect(() => {
-      window.scrollTo(0,0)
-      const runLocalData = async () => {
-
-      }
-      runLocalData()
-    })
+const PortofolioItem: FC<PortofolioItemProps> = ({ portofolio }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const runLocalData = async () => {
+      // Your asynchronous code here
+    };
+    runLocalData();
+  }, []); // Adding an empty dependency array to run the effect only once
 
   return (
-      <div  className="PortofolioItem">
-        <a href={portofolio.previewUrl} className="project1">         
-           <div className="desc">
-              <h3>{portofolio.title.toUpperCase()}</h3>
-              <p> Durée : <span>{portofolio.time}</span></p>
-                <p>{portofolio.description}</p>
-            </div>
+    <div className="PortofolioItem">
+      <a
+        href={portofolio.previewUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="project1"
+      >
+        <div className="desc">
+          <h3>{portofolio.title.toUpperCase()}</h3>
+          <p>
+            Durée : <span>{portofolio.time}</span>
+          </p>
+          <p>{portofolio.description}</p>
+        </div>
       </a>
-      </div>
+    </div>
   );
-}
+};
 
 export default PortofolioItem;
